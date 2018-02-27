84775c327a25a6b0ef4b3c2337af7cd2751a85e7
Clement-Bresson <clement0bresson@gmail.com>
2018-02-27 13:47:33 +0000

    getQueryParams

---
+++ index.js
@@ -0,0 +1,11 @@
+import * as R from 'ramda'
+
+export const getQueryParams = R.converge(R.compose(R.prop(1), R.find), [
+  R.compose(R.propEq('0'), R.nthArg(1)),
+  R.compose(
+    R.map(R.split('=')),
+    R.split('&'),
+    R.invoker(1, 'substr')(1),
+    R.nthArg(0)
+  )
+])
